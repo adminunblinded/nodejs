c7e115d182314a4f110670624767aef70aa89645
adminunblinded <158335079+adminunblinded@users.noreply.github.com>
2024-07-15 10:08:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
